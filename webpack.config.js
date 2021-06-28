90073eb092101f15a8102b62e84ba013025d36e3
OTree <kevin10410@gmail.com>
2021-06-28 11:16:48 +0800

    chore(webpack.config): refine structure of config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
   devtool: 'inline-source-map',
   devServer: {
     contentBase: "dist",
