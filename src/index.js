b5d885605a01ab6fb04c8adccae32203cd6d0a74
OTree <kevin10410@gmail.com>
2021-06-29 17:14:41 +0800

    feat(index.js): replace with arrow function to check babel-loader

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import _ from 'lodash';
 import './index.styl';
 import Icon from './assets/images/icon.svg';
 
-function image() {
+const image = () => {
   const myImage = document.createElement('img');
   myImage.src = Icon;
 
   return myImage;
 }
 
-function component() {
+const component = () => {
   const element = document.createElement('h1');
 
   // Lodash, currently included via a script, is required for this line to work
